edfa4666f735c5cc71232352c87d607d599d53fa
ali.ehyaie <ali.ehyaie@snapp.cab>
2023-12-03 23:36:51 +0330

    feat: add colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:{
+          main: '#22a958',
+          text: '#ffffff'
+        },
+        secondary: {
+          main: 'tomato'
+        },
+        disabled:{
+          main: '#252a3c1f',
+          text:'#252a3c61'
+        }
+      }
+    },
   },
   plugins: [],
 }
